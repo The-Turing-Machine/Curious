695da1f603e2317ff238277695d474ff464147a1
ayush <ayushkumarsingh97@gmail.com>
2016-06-11 20:37:42 +0530

    changes

--- twitter.py
+++ twitter.py
@@ -28,6 +28,8 @@ CONSUMER_SECRET = "JQ5E8Wn0XutYqslBdjGRJxi5snbh2bDmGAeq3mI8bXnmyYVVAn"
 # CONSUMER_KEY="yWOim0eBDzMrN8FlOilcRTVCK"
 # CONSUMER_SECRET = "4iGTjaL0r9slwCtU5jcQDdqIxvfYsp62Si71128eCJpd3jRJnw"
 
+CONSUMER_KEY="9u8p3OjDgDvFjTQQQ5XYQqB8v"
+CONSUMER_SECRET = "sy82wTf1VlTiCPtdM2BDOYNGRX9aTzrssi9M0IqeNX1lmUBK2M"
 
 
 
@@ -40,8 +42,8 @@ CONSUMER_SECRET = "JQ5E8Wn0XutYqslBdjGRJxi5snbh2bDmGAeq3mI8bXnmyYVVAn"
 # OAUTH_TOKEN="4653955494-VRs3MN2aPRhiQFBIdPyMs8S68X1cxaBT81s0bY7"
 # OAUTH_TOKEN_SECRET="2eAn9kfBofZpuFHuYaakbwbNqrpeesInMas1AAdkxQKb1"
 
-OAUTH_TOKEN= "4653955494-2z4klWrjEk8PhV1HCT9QFvibjPouDuj0z2YIQh6"
-OAUTH_TOKEN_SECRET="Kb01dB5abJyhW7to5rYRB9CTW0LwDwG6F4mOUMEQlTxYw"
+# OAUTH_TOKEN= "4653955494-2z4klWrjEk8PhV1HCT9QFvibjPouDuj0z2YIQh6"
+# OAUTH_TOKEN_SECRET="Kb01dB5abJyhW7to5rYRB9CTW0LwDwG6F4mOUMEQlTxYw"
 
 # OAUTH_TOKEN = "4653955494-7OzM7jwuYoAxVFDAiICYRf4yQP0y5XGItr1G6ii"
 # OAUTH_TOKEN_SECRET ="haXwpryYzHoPrOLxNVjGAhRuImvhPdjcw3KfBlQlyIc8R"
@@ -56,6 +58,11 @@ OAUTH_TOKEN_SECRET="Kb01dB5abJyhW7to5rYRB9CTW0LwDwG6F4mOUMEQlTxYw"
 # OAUTH_TOKEN =  "4653955494-HI5zQDkJD384A1kq0aXKLtcMFtdsQmtob7pcqgU"
 # OAUTH_TOKEN_SECRET = "g7d049GSwqmAEBI0aSsBTXpWGfweOYIpTt1ktW8j1ZbhU"
 
+OAUTH_TOKEN =  "4653955494-vi3R1KMecUKtKu27mmqMfAuMwGpYjEcKvXawSfy"
+OAUTH_TOKEN_SECRET =  "esHifHRZcQgGJPGfgaZmGcjlUd23IbdjuvbdOSko3CJub"
+
+
+s = requests.session()
 
 
 
@@ -64,7 +71,8 @@ OAUTH_TOKEN_SECRET="Kb01dB5abJyhW7to5rYRB9CTW0LwDwG6F4mOUMEQlTxYw"
 
 coun = ["Afghanistan","Argentina","Armenia","Australia","Austria","Bahrain","Canada","Central African Republic","Chad","Chile","China","Colombia","Comoros","Congo","Costa Rica","Croatia","Cuba","Cyprus","Czech Republic","North Korea","Democratic Republic of the Cong","Denmark","Djibouti","Dominica","Dominican Republic","Ecuador","Egypt","El Salvador","Equatorial Guinea","Eritrea","Estonia","Ethiopia","Fiji","Finland","France","Gabon","Gambia","Georgia","Germany","Ghana","Greece","Grenada","Guatemala","Guinea","Guinea-Bissau","Guyana","Haiti","Honduras","Hungary","Iceland","India","Indonesia","Iran","Iraq","Ireland","Israel","Italy","Jamaica","Japan","Jordan","Kazakhstan","Kenya","Kiribati","Kuwait","Kyrgyzstan","Latvia","Lebanon","Lesotho","Liberia","Libya","Liechtenstein","Lithuania","Luxembourg","Macedonia","Madagascar","Malawi","Malaysia","Maldives","Mali","Malta","Marshall Islands","Mauritania","Mauritius","Mexico","Micronesia","Monaco","Mongolia","Montenegro","Morocco","Mozambique","Myanmar","Namibia","Nauru","Nepal","Netherlands","New Zealand","Nicaragua","Niger","Nigeria","Norway","Oman","Pakistan","Palau","Panama","Papua New Guinea","Paraguay","Peru","Philippines","Poland","Portugal","Qatar","Republic of Korea (South Korea)","Republic of Moldova","Romania","Russian Federation","Rwanda","Saint Kitts and Nevis","Saint Lucia","Saint Vincent and the Grenadines","Samoa","San Marino","Sao Tome and Principe","Saudi Arabia","Senegal","Serbia","Seychelles","Sierra Leone","Singapore","Slovakia","Slovenia","Solomon Islands","Somalia","South Africa","South Sudan","Spain","Sri Lanka","Sudan","Suriname","Swaziland","Sweden","Switzerland","Syrian Arab Republic","Tajikistan","Thailand","Timor-Leste","Togo","Tonga","Trinidad and Tobago","Tunisia","Turkey","Turkmenistan","Tuvalu","Uganda","Ukraine","United Arab Emirates","United Kingdom of Great Britain and Northern Ireland","United Republic of Tanzania","United States of America","Uruguay","Uzbekistan","Vanuatu","Venezuela","Vietnam","Yemen","Zambia","Zimbabwe"]
 
-woeid =[u'23424740', u'23424747', u'23424748', u'23424750', u'23424775', u'23424782', u'23424787', u'23424868', u'23424796', u'23424800', u'23424801', u'23424802', u'23424819', u'23424829', u'23424824', u'23424833', u'83123', u'23424848', u'23424846', u'23424803', u'23424852', u'23424853', u'23424856', u'23424874', u'23424900', u'23424909', u'23424908', u'23424898', u'23424922', u'23424919', u'23424934', u'23424923', u'23424925', u'23424868', u'23424936', u'1062617', u'23424950', u'23424954', u'23424957', u'23424969', u'23424976', u'23424738', u'23424982', u'23424984']
+woeid =
+[u'23424740', u'23424747', u'23424748', u'23424750', u'23424775', u'23424782', u'23424787', u'23424868', u'23424796', u'23424800', u'23424801', u'23424802', u'23424819', u'23424829', u'23424824', u'23424833', u'83123', u'23424848', u'23424846', u'23424803', u'23424852', u'23424853', u'23424856', u'23424874', u'23424900', u'23424909', u'23424908', u'23424898', u'23424922', u'23424919', u'23424934', u'23424923', u'23424925', u'23424868', u'23424936', u'1062617', u'23424950', u'23424954', u'23424957', u'23424969', u'23424976', u'23424738', u'23424982', u'23424984']
 
 
 
@@ -113,40 +121,22 @@ dic = {}
 def get_trend_hashtags(woeid):
         for i in woeid:
             # try:
-            b = requests.get(url="https://api.twitter.com/1.1/trends/place.json?id="+i+"", auth=oauth)
+            b = s.get(url="https://api.twitter.com/1.1/trends/place.json?id="+i+"", auth=oauth)
             # print b.json()
             tag =[]
-            # try:
+            try:
             # pp.pprint(b.json())
-            for k in b.json()[0]['trends']:
-                tag.append(k['query'])
+                for k in b.json()[0]['trends']:
+                    tag.append(k['query'])
 
-                # print k['query']
-            # print tag[0:10]
-            dic[i] = tag[0:10]
-            print dic
-                # dic[i] = tag
-                # print dic
-                # print pp.pprint(b.json()[0]['trends'])
-                # for j in b.json()[0]['trends']["query"]:
-                #     tag.append(j)
-                # dic[i] = tag
-                # print dic
-            # except Exception as e:
-            #     print "error"
-                # print e.message
+                    # print k['query']
+                # print tag[0:10]
+                dic[i] = tag[0:10]
+                print dic
+            except:
+                print "error"
 
-                # woeid.remove(i)
-                # print i
-                # print woeid
-                # print b.json()
 
-            # for i in b.json()[0]:
-            #     tag.append(i['trends']["query"])
-            # dic[i] = tag
-            # print dic
-            # except:
-            #     print "not available"
 
 
 
