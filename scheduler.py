4beedf4006a1b6ef99481dea1732f492ab8fc171
arsh23 <programmer.arsh@gmail.com>
2016-06-11 19:04:19 +0530

    async testing

--- scheduler.py
+++ scheduler.py
@@ -5,15 +5,24 @@ import time
 import requests
 import gevent.pool
 import gevent.queue
+import gevent
 
-def a():
-    print 'func a'
-    for _ in range(20):
-        f = b()
-        queue.put(f)
+s = requests.Session()
 
-def b():
-    print 'func b'
+def a(y):
+    print 'func a',y
+    for x in range(500):
+        f = b
+        queue.put((f,x))
+    # print queue
+
+def b(x):
+    # gevent.sleep(2)
+    r = s.get("https://post-cache.tagboard.com/search/euro2016?excluded_networks=facebook&count=100")
+    print r.status_code
+
+    print 'func b - ',x
+
 def c():
     print 'func c'
 
@@ -22,13 +31,30 @@ pool = gevent.pool.Pool(max_workers)
 queue = gevent.queue.Queue()
 
 def func():
-    function = queue.get_nowait()
-    function()
+    t = queue.get_nowait()
+    function = t[0]
+    param = t[1]
+    print param
+    function(param)
 
 
-f = a()
-queue.put(f)
+f = a
+# f()
+# queue.put((f,'none'))
+# queue.put((f,'none'))
+# queue.put((f,'none'))
+# queue.put((f,'none'))
+pool.start(pool.spawn(a,'none'))
+pool.join()
 
-while not queue.empty() and not pool.free_count() == max_workers:
+
+# print 'while',not queue.empty() and not pool.free_count() == max_workers
+while not queue.empty() and not pool.full():
     for x in xrange(0, min(queue.qsize(), pool.free_count())):
-        pool.start(pool.spawn(func))
+        t = queue.get_nowait()
+        print '-----'
+        # print 'yo',t,pool.free_count()
+        pool.start(pool.spawn(t[0],t[1]))
+
+
+pool.join()
