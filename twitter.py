c13029e051395668abca2b1d933a9dc8ea9369dc
ayush <ayushkumarsingh97@gmail.com>
2016-06-11 17:50:50 +0530

    oauth adde

--- twitter.py
+++ twitter.py
@@ -12,25 +12,36 @@ ACCESS_TOKEN_URL = "https://api.twitter.com/oauth/access_token"
 # CONSUMER_KEY = "rcPzx7rJtywQMk0VmdGLCAxjb"
 # CONSUMER_SECRET = "DE3J8NLlSVSnGJuQ0U3GKCBjrIo7vBxQCEeCOj1HoS2bP74NPv"
 
-CONSUMER_KEY = "N8CefcBhNEgLLzj5C5NTkwk7I"
-CONSUMER_SECRET = "XvalzhATRiMwtRmz5g6jrU4yI9VrMvGRbPYpWaXYtPEofMYyMT"
+# CONSUMER_KEY = "N8CefcBhNEgLLzj5C5NTkwk7I"
+# CONSUMER_SECRET = "XvalzhATRiMwtRmz5g6jrU4yI9VrMvGRbPYpWaXYtPEofMYyMT"
 
 # CONSUMER_KEY="XX254OwNWCuctNrlo29GW2Ihx"
 # CONSUMER_SECRET = "KDTb6Q4swNH1ThMO13SH9vzhOQecP9705wFXw6qPg90S52ISZU"
+
+CONSUMER_KEY="gcLU6rXGwRHUaWOSl9J648bkp"
+CONSUMER_SECRET = "JQ5E8Wn0XutYqslBdjGRJxi5snbh2bDmGAeq3mI8bXnmyYVVAn"
+
 
 
 
 # OAUTH_TOKEN = "4653955494-jbxlbxolDzQv16TA6astzszHOT7HHJbtZTy5HNV"
 # OAUTH_TOKEN_SECRET = "3ndOvcdpb79OR73BfJtJ64i9Qg6X8qWIpcI4SAcnIfKSB"
 
-OAUTH_TOKEN ="4653955494-Cbul45ZQvzsK16aELSN2O6ecVaT4UbSDujfCkqx"
-OAUTH_TOKEN_SECRET = "8CwjuAi7ctdJQIfW4a835IdLYcap8cl5V2ZPaG0rm7exW"
+# OAUTH_TOKEN ="4653955494-Cbul45ZQvzsK16aELSN2O6ecVaT4UbSDujfCkqx"
+# OAUTH_TOKEN_SECRET = "8CwjuAi7ctdJQIfW4a835IdLYcap8cl5V2ZPaG0rm7exW"
 
 # OAUTH_TOKEN="4653955494-VRs3MN2aPRhiQFBIdPyMs8S68X1cxaBT81s0bY7"
 # OAUTH_TOKEN_SECRET="2eAn9kfBofZpuFHuYaakbwbNqrpeesInMas1AAdkxQKb1"
 
+OAUTH_TOKEN= "4653955494-2z4klWrjEk8PhV1HCT9QFvibjPouDuj0z2YIQh6"
+OAUTH_TOKEN_SECRET=" Kb01dB5abJyhW7to5rYRB9CTW0LwDwG6F4mOUMEQlTxYw"
 
-woeid=['23424739', '23424742', '23424740', '23424744', '23424745', '23424737', '23424747', '23424743', '23424748', '23424750', '23424741', '23424758', '23424753', '23424759', '23424754', '23424765', '23424757', '23424760', '23424764', '23424770', '23424762', '23424761', '23424755', '23424768', '23424773', '23424771', '23424978', '23424774', '23424794', '23424776', '23424785', '23424775', '23424792', '837054', '23424782', '23424971', '23424787', '23424786', '23424779', '879050', '23424843', '23424793', '26812346', '23424810', '23424868', '2480268', '23424796', '2345158', '23424798', '23424800', '23424801', '23424802', '23424807', '23424835', '23424806', '23424805', '23424808', '23424813', '23424812', '23424819', '1329894', '1259029', '2347569', '23424829', '23424824', '23424833', '2414888', '83123', '23424835', '1559861', '23424836', '23424839', '23424841', '23424844', '23424845', '23424848', '23424846', '23424851', '2124082', '23424803', '23424852', '23424853', '23424858', '23424856', '2429994', '23424871', '23424863', '23424867', '1940631', '23424864', '23424874', '2437673', '23424880', '23424876', '23424882', '23424879', '23424875', '979721', '2443718', '23424883', '23424889', '23424901', '23424899', '23424891', '23424897', '23424932', '23424896', '23424894', '23424900', '24865720', '483301', '23424887', '20069817', '23424893', '23424902', '23424763', '23424987', '23424912', '23424911', '23424909', '23424916', '23424906', '23424908', '29145317', '23424898', '23424922', '23424927', '159386', '12491802', '23424917', '23424919', '23424934', '23424923', '23424925', '28395013', '23424868', '23424885', '23424933', '23424936', '23424937', '23424940', '1325621', '2347676', '23424992', '532373', '1441680', '29125356', '23424943', '20069818', '23424941', '1062844', '1062617', '23424877', '23424945', '23424766', '23424949', '24865670', '23424952', '23424950', '12467072', '905958', '23424913', '23424993', '23424954', '23424957', '23424956', '23424961', '23424960', '23424968', '1048627', '23424964', '23424958', '23424967', '23424969', '23424972', '23424970', '1301134', '23424976', '23424738', '20070563', '23424973', '23424977', '23424979', '23424980', '23424907', '23424982', '23424984', '23425002', '23425003', '23425004']
+
+
+
+coun = ["Afghanistan","Argentina","Armenia","Australia","Austria","Bahrain","Canada","Central African Republic","Chad","Chile","China","Colombia","Comoros","Congo","Costa Rica","Croatia","Cuba","Cyprus","Czech Republic","North Korea","Democratic Republic of the Cong","Denmark","Djibouti","Dominica","Dominican Republic","Ecuador","Egypt","El Salvador","Equatorial Guinea","Eritrea","Estonia","Ethiopia","Fiji","Finland","France","Gabon","Gambia","Georgia","Germany","Ghana","Greece","Grenada","Guatemala","Guinea","Guinea-Bissau","Guyana","Haiti","Honduras","Hungary","Iceland","India","Indonesia","Iran","Iraq","Ireland","Israel","Italy","Jamaica","Japan","Jordan","Kazakhstan","Kenya","Kiribati","Kuwait","Kyrgyzstan","Latvia","Lebanon","Lesotho","Liberia","Libya","Liechtenstein","Lithuania","Luxembourg","Macedonia","Madagascar","Malawi","Malaysia","Maldives","Mali","Malta","Marshall Islands","Mauritania","Mauritius","Mexico","Micronesia","Monaco","Mongolia","Montenegro","Morocco","Mozambique","Myanmar","Namibia","Nauru","Nepal","Netherlands","New Zealand","Nicaragua","Niger","Nigeria","Norway","Oman","Pakistan","Palau","Panama","Papua New Guinea","Paraguay","Peru","Philippines","Poland","Portugal","Qatar","Republic of Korea (South Korea)","Republic of Moldova","Romania","Russian Federation","Rwanda","Saint Kitts and Nevis","Saint Lucia","Saint Vincent and the Grenadines","Samoa","San Marino","Sao Tome and Principe","Saudi Arabia","Senegal","Serbia","Seychelles","Sierra Leone","Singapore","Slovakia","Slovenia","Solomon Islands","Somalia","South Africa","South Sudan","Spain","Sri Lanka","Sudan","Suriname","Swaziland","Sweden","Switzerland","Syrian Arab Republic","Tajikistan","Thailand","Timor-Leste","Togo","Tonga","Trinidad and Tobago","Tunisia","Turkey","Turkmenistan","Tuvalu","Uganda","Ukraine","United Arab Emirates","United Kingdom of Great Britain and Northern Ireland","United Republic of Tanzania","United States of America","Uruguay","Uzbekistan","Vanuatu","Venezuela","Vietnam","Yemen","Zambia","Zimbabwe"]
+
+woeid=['23424740', '23424747', '23424748', '23424750', '23424978', '23424774', '23424794', '23424776', '23424785', '23424775', '23424792', '837054', '23424782', '23424971', '23424787', '23424786', '23424779', '879050', '23424843', '23424793', '26812346', '23424810', '23424868', '2480268', '23424796', '2345158', '23424798', '23424800', '23424801', '23424802', '23424807', '23424835', '23424806', '23424805', '23424808', '23424813', '23424812', '23424819', '1329894', '1259029', '2347569', '23424829', '23424824', '23424833', '2414888', '83123', '23424835', '1559861', '23424836', '23424839', '23424841', '23424844', '23424845', '23424848', '23424846', '23424851', '2124082', '23424803', '23424852', '23424853', '23424858', '23424856', '2429994', '23424871', '23424863', '23424867', '1940631', '23424864', '23424874', '2437673', '23424880', '23424876', '23424882', '23424879', '23424875', '979721', '2443718', '23424883', '23424889', '23424901', '23424899', '23424891', '23424897', '23424932', '23424896', '23424894', '23424900', '24865720', '483301', '23424887', '20069817', '23424893', '23424902', '23424763', '23424987', '23424912', '23424911', '23424909', '23424916', '23424906', '23424908', '29145317', '23424898', '23424922', '23424927', '159386', '12491802', '23424917', '23424919', '23424934', '23424923', '23424925', '28395013', '23424868', '23424885', '23424933', '23424936', '23424937', '23424940', '1325621', '2347676', '23424992', '532373', '1441680', '29125356', '23424943', '20069818', '23424941', '1062844', '1062617', '23424877', '23424945', '23424766', '23424949', '24865670', '23424952', '23424950', '12467072', '905958', '23424913', '23424993', '23424954', '23424957', '23424956', '23424961', '23424960', '23424968', '1048627', '23424964', '23424958', '23424967', '23424969', '23424972', '23424970', '1301134', '23424976', '23424738', '20070563', '23424973', '23424977', '23424979', '23424980', '23424907', '23424982', '23424984', '23425002', '23425003', '23425004']
 
 def setup_oauth():
     """Authorize your app via identifier."""
@@ -70,24 +81,27 @@ def get_oauth():
                 resource_owner_secret=OAUTH_TOKEN_SECRET)
     return oauth
 
+dic = {}
 def get_trend_hashtags(woeid):
-        # for i in woeid:
-        #     # try:
-        #     b = requests.get(url="https://api.twitter.com/1.1/trends/place.json?id="+i+"", auth=oauth)
-        #     c = b.json()
-        #     d = []
-        #     d = c
-        #     print d[0]
-        #
-        #     #
-        #     # with open('data.txt', 'w') as outfile:
-        #     #     json.dump(c, outfile)
-        #     pp.pprint(b.json())
-        #     # except:
-        #     #     print "not available"
-        b = requests.get(url="https://api.twitter.com/1.1/trends/place.json?id=23424848", auth=oauth)
-        for i in b.json()[0]['trends']:
-            print i["query"]
+        for i in woeid:
+            # try:
+            b = requests.get(url="https://api.twitter.com/1.1/trends/place.json?id="+i+"", auth=oauth)
+            print b.json()
+            tag =[]
+            try:
+
+                print b.json()[0]['trends']
+            except Exception as e:
+                print i
+
+            # for i in b.json()[0]:
+            #     tag.append(i['trends']["query"])
+            # dic[i] = tag
+            # print dic
+            # except:
+            #     print "not available"
+
+
 
 
 
@@ -103,7 +117,7 @@ if __name__ == "__main__":
         print
     else:
         oauth = get_oauth()
-        get_trend_hashtags(woeid)
+        # get_trend_hashtags(woeid)
         # r = requests.get(url="https://api.twitter.com/1.1/search/tweets.json?q=messi&count=100", auth=oauth)
         # a = r.json()
         # print len(a)
