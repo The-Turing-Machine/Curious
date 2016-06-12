a8e6700e703447d0f2e19e338336374b33ec9b73
RahulBaboota <rahulbaboota08@gmail.com>
2016-06-12 05:52:09 +0530

    Fixed Bugs

--- SocialMedia/SocialMediaData.py
+++ SocialMedia/SocialMediaData.py
@@ -49,37 +49,54 @@ pp = pprint.PrettyPrinter(indent=4)
 #Function which pulls the data 
 def data_social_medias(hashtag):
 
-	r=requests.get("https://post-cache.tagboard.com/search/"+hashtag+"?count=100")
+    r=requests.get("https://post-cache.tagboard.com/search/"+hashtag+"?count=100")
 
-	social_media_data=json.loads(r.text)
-	no_of_posts=len(social_media_data['posts'])
+    social_media_data=json.loads(r.text)
+    no_of_posts=len(social_media_data['posts'])
 
-	for i in range(0,no_of_posts):
-		social_media_dictionary={}
+    for i in range(0,no_of_posts):
+        social_media_dictionary={}
 
-		try:
-			social_media_dictionary['text']=social_media_data['posts'][i]['text'].encode('ascii', errors='ignore')
-			social_media_dictionary['social_media_network']=social_media_data['posts'][i]['network']
-			social_media_dictionary['social_media_link']=social_media_data['posts'][i]['permalink']
-			social_media_dictionary['user_profile_image']=social_media_data['posts'][i]['user_profile_image_url'].replace("//","")
-			epoch_time=social_media_data['posts'][i]['post_time']
-			real_time=datetime.datetime.fromtimestamp(epoch_time).strftime("%c")
-			social_media_dictionary['time']=real_time
-			social_media_dictionary['post_date']=social_media_data['posts'][i]['post_date'][:-5]
-			social_media_dictionary['real_user_name']=social_media_data['posts'][i]['user_real_name'].encode('ascii', errors='ignore')
-			social_media_dictionary['photos']=social_media_data['posts'][i]['photos'].replace("//","")
-			social_media_dictionary['videos']=social_media_data['posts'][i]['videos']
-			social_media_dictionary['post_id']=social_media_data['posts'][i]['post_id']
-		except:
-			pass
+        try:
+            social_media_dictionary['text']=social_media_data['posts'][i]['text'].encode('ascii', errors='ignore')
+            social_media_dictionary['social_media_network']=social_media_data['posts'][i]['network']
+            social_media_dictionary['social_media_link']=social_media_data['posts'][i]['permalink']
+            social_media_dictionary['user_profile_image']=social_media_data['posts'][i]['user_profile_image_url'].replace("//","")
+            epoch_time=social_media_data['posts'][i]['post_time']
+            real_time=datetime.datetime.fromtimestamp(epoch_time).strftime("%c")
+            social_media_dictionary['time']=real_time
+            social_media_dictionary['post_date']=social_media_data['posts'][i]['post_date'][:-5]
+            social_media_dictionary['real_user_name']=social_media_data['posts'][i]['user_real_name'].encode('ascii', errors='ignore')
+            social_media_dictionary['videos']=social_media_data['posts'][i]['videos']
+            social_media_dictionary['post_id']=social_media_data['posts'][i]['post_id']
 
-		all_social_media_link.append(social_media_dictionary)
+            videos_link=[]
+            videos_list=social_media_data['posts'][1]['videos']
+            keys2=videos_list[0].keys()
+            del keys2[0]
+            for key in keys2:
+                videos_list[0][key].replace("//","")
+                videos_link.append(photos_list[0][key])
+            social_media_dictionary['videos']=videos_link
 
+            photos_link=[]
+            photos_list=social_media_data['posts'][1]['photos']
+            keys1=photos_list[0].keys()
+            del keys1[0]
+            for key in keys1:
+                photos_list[0][key].replace("//","")
+                photos_link.append(photos_list[0][key])
+            social_media_dictionary['photos']=photos_link
+
+        except:
+            pass
+
+        all_social_media_link.append(social_media_dictionary)
 
 for hashtag in hashtags:
-	data_social_medias(hashtag)
+    data_social_medias(hashtag)
 
-pp.pprint(all_social_media_link)	
+pp.pprint(all_social_media_link)    
 print len(all_social_media_link)
 
 
