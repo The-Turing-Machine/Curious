312ba7b404fb6902cb245603916f1e5144b2f4c7
thinker3197 <ashishgupta.3197@gmail.com>
2016-06-12 09:04:04 +0530

    Added changes

--- app.py
+++ app.py
@@ -29,5 +29,5 @@ def send_data():
 if __name__ == "__main__":
     thread_main = Thread()
     thread_main.start()
-	port = int(os.environ.get('PORT', 5000))
-	app.run(debug=True,threaded=True,host='0.0.0.0',port=port)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True,threaded=True,host='0.0.0.0',port=port)
